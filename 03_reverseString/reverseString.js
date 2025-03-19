6daa6e96cf147a5b07f30800ab9561f5683cb19b
Luiz Antonio <l.antonio4100@gmail.com>
2025-03-19 15:52:13 -0300

    Exércicio 3 completo

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var newString = "";
 
+    for (var i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+
+    return newString;
 };
+
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
